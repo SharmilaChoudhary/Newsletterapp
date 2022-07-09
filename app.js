9af79255238083351ea6f839f745b6f485557b2d
Sharmila_21 <shamuu2002@gmail.com>
2022-07-09 13:56:33 +0530

    NewsletterApp

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express =require("express");
 const bodyParser=require("body-parser");
 const request=require("request");
-const https=require("https");
 const app=express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,70 +10,17 @@ app.get("/",function(req,res){
 });
 
 app.post("/",function(req,res){
-    const firstname=req.body.fn;
-    const lastname=req.body.ln;
-    const email=req.body.email;
-
-    const data={
-        members:[
-           {
-            email_address:email,
-            status:"subscribed",
-            merge_fields:{
-                FNAME:firstname,
-                LNAME:lastname
-            }
-           } 
-        ]
-    };
-
-    const jsondata=JSON.stringify(data);
-    const url="https://us11.api.mailchimp.com/3.0/lists/dc3e47e709";   
-    const options={
-        method:"POST",
-        auth:"sharmila:fd94cc755d0122dd7fef1c2135cab024-us11"
-    }
-    
-    
-   const request= https.request(url,options,function(response){
-    if(response.statusCode===200)
-    {
-        res.sendFile(__dirname+"/success.html");
-    }
-    else{
-        res.sendFile(__dirname+"/failure.html");
-    }
-     response.on("data",function(data){
-        console.log(JSON.parse(data));
-       
-     })
-    })
-
-
-    request.write(jsondata);
-    request.end();
- 
-
+    var firstname=req.body.fn;
+    var lastname=req.body.ln;
+    var email=req.body.email;
+    console.log(firstname,lastname,email);
 });
-
-
-app.post("/failure",function(req,res){
-res.redirect("/");
-}
-);
-app.listen(process.env.PORT || 3000,function(){
+app.listen("3000",function(){
     console.log("you server is  runing at port 3000");
+    console.log(__dirname);
 });
 
 
 
-
-
-
-
-// api key
+// apikey
 // 433c402ccf8e47c362c58133aae7111a-us11
-
-
-// list_id
-// dc3e47e709
